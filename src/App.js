359edd558c4db30a6f9ae0d4eb1bd4e3c62e2aa9
Stephanie Liu <yliu@Stephanies-MacBook-Air.local>
2021-08-19 11:42:46 -0500

    display frontend

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
 import './App.css';
+import { Route, Link} from "react-router-dom";
+import React, {useState, useEffect} from "react";
+import Display from './Display'
+import Form from './Form'
 
 function App() {
+  const url = "https://group-mern-lab-628.herokuapp.com";
+  const [songs,setSongs] =useState([])
+  const getSongs = ()=>{ 
+    fetch(url + "/songs")
+    .then((response) => response.json())
+    .then((data) => setSongs(data))
+  }
+
+  useEffect(() => getSongs(), []);
+  
+  
   return (
     <div className="App">
+        <img className ="logo" src="https://res.cloudinary.com/dt5zbnwvr/image/upload/v1629386427/Navy_Minimalist_Music_Logo_3_t74eqs.png"></img>
+     <hr></hr>
+     <main>
+     <h1>PLAYLIST 1</h1>  
+     <Route exact path="/"
+            render={(rp) => <Display {...rp} songs={songs} 
+            />}
+    />
+     <Form/>
+     </main>
     </div>
   );
 }
---
+++ src/Display.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Display = (props) => {
+    const { songs } = props;
+
+ const loaded = () => (
+      <div className="playlist">
+      {songs.map((song) => (
+         <article>
+          <p id="title">{song.title}</p>
+          <p id="artist">{song.artist}</p>
+           <p id="time">{song.time}</p>
+          {/* <button onClick={() => {
+            props.deleteSong(song)
+          }}>Delete</button>  */}
+        </article>
+     ))}
+        </div>
+    )
+       
+const loading = <h1>Loading...</h1>
+
+return songs.length > 0 ? loaded() : loading;
+ };
+
+export default Display; 
